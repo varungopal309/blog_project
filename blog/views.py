9d28490336b56d268c979a2dce470956e1f244eb
varun gopal <varungopal37@gmail.com>
2022-04-24 02:26:24 +0530

    added profile

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.views.generic import(
+ ListView,
+ DetailView, 
+ CreateView,
+ UpdateView,
+ DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from .models import Post
 # Create your views here.
-post = [ {'author':'varun',
-               'content':'django_project',
-               'date_added':'28-1-2018',
-               'title':'blog 1'},
-               {'author':'gopal',
-               'content':'python_tutorial',
-               'date_added':'29-1-2018',
-               'title':'blog 2'},
-              
-]
+
 def home(request):
     context = {'post':Post.objects.all()}
     return render(request,'blog/home.html',context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name= 'post'
+    ordering = ['-date_added']
+    paginate_by = 5
+ 
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name= 'post'
+    #ordering = ['-date_added']
+    paginate_by = 5
+    def get_queryset(self):
+         user = get_object_or_404(User,username=self.kwargs.get('username'))
+         return Post.objects.filter(author=user).order_by('-date_added')
+    
+    
+class PostDetailView(DetailView):
+    model =Post
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+    
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    model =Post
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+    
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+       post = self.get_object()
+       if self.request.user == post.author:
+            return True
+       return False
+        
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin,DeleteView):
+    model =Post
+    success_url = '/'
+    def test_func(self):
+       post = self.get_object()
+       if self.request.user == post.author:
+            return True
+       return False       
 
 def about(request):
     return render(request,'blog/about.html',{'title':'about'})
--- django_project/settings.py
+++ django_project/settings.py
@@ -127,4 +127,11 @@ MEDIA_URL ='/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD= os.environ.get('EMAIL_PASS')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
